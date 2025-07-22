53d7b0b2d9ba63098a4b6c946bf1227fb35cecbd
Jaydeepsinh Parmar <148114163+jaydeep099@users.noreply.github.com>
2025-07-23 00:02:33 +0530

    Navbar is completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import Navbar from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col bg-gradient-to-b from-[#151515] to-[#000000] h-screen w-full">
-      <Navbar/>
-      <main className="flex flex-col">        
+    <div className="flex flex-col bg-gradient-to-b from-[#151515] to-[#000000] h-screen w-full relative">
+      <div className="w-15 h-[725px] absolute -top-[345px] left-[750px] bg-gradient-to-r from-[#D0A704] to-[#d0a70446] rotate-90 rounded-4xl blur-[100px]"></div>
+      <Navbar />
+      <main className="flex flex-col w-full relative top-[72px] p-6 gap-6">
+        <section className="flex flex-row  opacity-100 gap-6"></section>
+        <section className="flex flex-col opacity-100 gap-2"></section>
       </main>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { ChevronDown } from "lucide-react";
+
 export default function Navbar() {
   return (
-    <div className="fixed w-full h-[76px] border-b-[1px] flex flex-row items-center justify-between pt-4 pb-3 px-6 ">
+    <div className="fixed w-full h-[76px] border-b-[1px] flex flex-row items-center justify-between pt-4 pb-3 px-6">
       <div className="flex flex-col h-[26px] gap-2">
         <div className="flex flex-row h-[26px] gap-2.5">
           <div className="h-[26px] w-5 relative">
@@ -22,20 +25,87 @@ export default function Navbar() {
         </div>
       </div>
       <div className="flex flex-row w-[540px] h-9 gap-4">
-        <button className="flex flex-row w-[110px] h-[36px] rounded-[6px] py-2.5 px-3 gap-[6-x]">
-            <div className="w-4 h-4">
-                <img src="./dashboard.png" alt="dashboard" className="w-3 h-3 top-0.5 left-0.5"/>
-            </div>
-            <div className="w-12 h-14 text-[#ffffff] font-bold text-[12px] leading-[120%] text-center tracking-[-1%]">
-               Dashboard
-            </div>
+        <button className="flex flex-row w-[110px] h-[36px] rounded-[6px] py-2.5 px-3 gap-[6px]">
+          <div className="w-4 h-4">
+            <img
+              src="./dashboard.svg"
+              alt="dashboard"
+              className="w-3 h-3 top-0.5 left-0.5"
+            />
+          </div>
+          <div className="w-12 h-[14px] text-[#ffffff] font-bold text-[12px] leading-[120%] text-center tracking-[-1%]">
+            Dashboard
+          </div>
         </button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+        <button className="flex flex-row w-[98px] h-[36px] rounded-[6px] py-2.5 px-3 gap-[6px]">
+          <div className="w-4 h-4">
+            <img
+              src="./cctv.svg"
+              alt="cctv"
+              className="w-3 h-3 top-0.5 left-0.5"
+            />
+          </div>
+          <div className="w-[52px] h-[14px] text-[#ffffff] font-bold text-[12px] leading-[120%] text-center tracking-[-1%]">
+            Camera
+          </div>
+        </button>
+        <button className="flex flex-row w-[89px] h-[36px] rounded-[6px] py-2.5 px-3 gap-[6px]">
+          <div className="w-4 h-4">
+            <img
+              src="./scenes.svg"
+              alt="scenes"
+              className="w-3 h-3 top-0.5 left-0.5"
+            />
+          </div>
+          <div className="w-[43px] h-[14px] text-[#ffffff] font-bold text-[12px] leading-[120%] text-center tracking-[-1%]">
+            Scenes
+          </div>
+        </button>
+        <button className="flex flex-row w-[100px] h-[36px] rounded-[6px] py-2.5 px-3 gap-[6px]">
+          <div className="w-4 h-4">
+            <img
+              src="./incidents.svg"
+              alt="incidents"
+              className="w-[14px] h-3 top-0.5 left-[1px]"
+            />
+          </div>
+          <div className="w-[54px] h-[14px] text-[#ffffff] font-bold text-[12px] leading-[120%] text-center tracking-[-1%]">
+            Incidents
+          </div>
+        </button>
+        <button className="flex flex-row w-[79px] h-[36px] rounded-[6px] py-2.5 px-3 gap-[6px]">
+          <div className="w-4 h-4">
+            <img
+              src="./users.svg"
+              alt="users"
+              className="w-[13px] h-[10px] top-[2.67px] left-[1.33px]"
+            />
+          </div>
+          <div className="w-[33px] h-[14px] text-[#ffffff] font-bold text-[12px] leading-[120%] text-center tracking-[-1%]">
+            Users
+          </div>
+        </button>
       </div>
-      <div></div>
+      <div className="flex flex-row w-[200px] h-[48px] gap-2 p-2 rounded-md">
+        <div className="flex flex-row w-8 h-8">
+          <img
+            src="./user-avatar.svg"
+            alt="user-avatar"
+            className="w-8 h-8 opacity-[1] rounded-full"
+          />
+        </div>
+        <div className="flex flex-col w-[120px] h-[32px] gap-[2]">
+          <p className="w-[124px] h-[14px] font-sans font-semibold text-sm tracking-normal leading-[100%]">
+            Mohammed Ajhas
+          </p>
+          <p className="w-[124px] h-[16px] font-sans font-normal text-xs leading-4 tracking-normal">
+            ajhas@mandlac.com
+          </p>
+        </div>
+        <div className="w-4 h-4 relative">
+          <ChevronDown />
+        </div>
+      </div>
     </div>
   );
 }
